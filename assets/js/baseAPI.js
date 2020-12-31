4933e29134a9a99438d067df9375befb1740db1e
A-jiasong <1370402406@qq.com>
2020-12-31 16:23:09 +0800

    基本资料的实现

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,24 @@
+// 只要一调用ajax请求就会调用ajaxPrefilter这个函数
 $.ajaxPrefilter(function(options) {
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    // headers就是请求头配置对象
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        // console.log('执行了 complete 回调：')
+        // console.log(res)
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 强制清空 token
+            localStorage.removeItem('token')
+                // 2. 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
